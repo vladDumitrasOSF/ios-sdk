5987f99a05d32ac05fbec49adc5e32482184573d
vladDumitrasOSF <DumitrasVlad@I-Apple-3.local>
2013-10-18 13:57:11 +0300

    modified_since example elements added;

--- Examples/CTCTContact/CTCTContact/CollectionsViewController.m
+++ Examples/CTCTContact/CTCTContact/CollectionsViewController.m
@@ -9,21 +9,40 @@
 #import "CollectionsViewController.h"
 
 #import "ContactsCollection.h"
+#import "ListsCollection.h"
+#import "EmailCampaignService.h"
 #import "LoadingView.h"
 #import "CTCTGlobal.h"
 #import "ResultSet.h"
 
 @interface CollectionsViewController ()<UITableViewDataSource,UITableViewDelegate,UITextFieldDelegate,UIGestureRecognizerDelegate>
 {
-    NSString *status;
-    NSArray *resultsArray;
+    NSString        *status;
+    int             selectedCall;
+    NSArray         *resultsArray;
+    NSDateFormatter *dateFormat;
     
     LoadingView *loadingView;
 }
+
+typedef enum
+{
+    CONTACT_CALL = 1,
+    LIST_CALL = 2,
+    EMAIL_CALL = 3,
+    MEMBERSHIP_CALL = 4
+}CALL_ENUM;
+
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UILabel *statusLable;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UITextField *dateTextField;
 
+@property (strong, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UIButton *activeBtn;
+@property (weak, nonatomic) IBOutlet UIButton *unconfBtn;
+@property (weak, nonatomic) IBOutlet UIButton *outputBtn;
+@property (weak, nonatomic) IBOutlet UIButton *removedBtn;
 @end
 
 @implementation CollectionsViewController
@@ -52,7 +71,6 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
     }
     return self;
 }
@@ -65,6 +83,7 @@
     self.tableView.delegate = self;
     
     self.textField.delegate = self;
+    self.dateTextField.delegate = self;
     
     resultsArray = [[NSArray alloc]init];
     
@@ -72,6 +91,12 @@
     [self.view addGestureRecognizer:tap];
     
     loadingView = [[LoadingView alloc]initWithFrame:self.view.frame];
+    
+    dateFormat = [[NSDateFormatter alloc]init];
+    [dateFormat setDateFormat:@"yyyy-MM-dd"];
+    [dateFormat setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+    
+    selectedCall = 0;
 }
 
 - (void)didReceiveMemoryWarning
@@ -83,6 +108,12 @@
     [self setTableView:nil];
     [self setStatusLable:nil];
     [self setTextField:nil];
+    [self setDateTextField:nil];
+    [self setDatePicker:nil];
+    [self setActiveBtn:nil];
+    [self setUnconfBtn:nil];
+    [self setOutputBtn:nil];
+    [self setRemovedBtn:nil];
     [super viewDidUnload];
 }
 
@@ -99,10 +130,25 @@
     
     if(cell == nil)
         cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+   
+    if(selectedCall == CONTACT_CALL || selectedCall == 0 || selectedCall == MEMBERSHIP_CALL)
+    {
     Contact *cont = resultsArray[indexPath.row];
     
     cell.textLabel.text =[NSString stringWithFormat:@"%@ %@",cont.firstName,cont.lastName];
-    
+    }
+    else if(selectedCall == LIST_CALL)
+    {
+        ContactList *cont = resultsArray[indexPath.row];
+        
+        cell.textLabel.text =[NSString stringWithFormat:@"%@ %@",cont.name,cont.status];
+    }
+    else if(selectedCall == EMAIL_CALL)
+    {
+        EmailCampaign *cont = resultsArray[indexPath.row];
+        
+        cell.textLabel.text =[NSString stringWithFormat:@"%@ %@",cont.name,cont.status];
+    }
     return cell;
 }
 
@@ -118,12 +164,18 @@
 
 - (void)startCall
 {
-   self.statusLable.text = status;
+    selectedCall = 0;
+    self.statusLable.text = status;
+    self.dateTextField.text = @"";
     [loadingView showLoadingInView:self.view];
     dispatch_queue_t callService = dispatch_queue_create("callService", nil);
     dispatch_async(callService, ^{
         
-        HttpResponse *response = [ContactsCollection contactsWithAccessToken:[CTCTGlobal shared].token andStatus:status withAlimitOf:self.textField.text];
+        HttpResponse *response = nil;
+        if(self.textField.text.length > 0)
+            response = [ContactsCollection contactsWithAccessToken:[CTCTGlobal shared].token andStatus:status withAlimitOf:self.textField.text];
+        else 
+            response = [ContactsCollection contactsWithAccessToken:[CTCTGlobal shared].token andStatus:status withAlimitOf:nil];
         
         dispatch_async(dispatch_get_main_queue(), ^{
             
@@ -143,5 +195,184 @@
 - (void)CloseKeys
 {
     [self.view endEditing:YES];
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    if(textField.tag == 1)
+    {
+        textField.inputView = self.datePicker;
+        self.dateTextField.text = [dateFormat stringFromDate:[self.datePicker date]];
+        self.textField.text = @"";
+        
+        self.activeBtn.enabled = NO;
+        self.unconfBtn.enabled = NO;
+        self.outputBtn.enabled = NO;
+        self.removedBtn.enabled = NO;
+    }
+    self.dateTextField.text = @"";
+    self.activeBtn.enabled = YES;
+    self.unconfBtn.enabled = YES;
+    self.outputBtn.enabled = YES;
+    self.removedBtn.enabled = YES;
+}
+- (IBAction)onSelectedDate:(id)sender
+{
+    self.dateTextField.text = [dateFormat stringFromDate:[self.datePicker date]];  
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    if(textField.tag == 1 && textField.text.length > 0)
+    {
+        [self callForDate];
+    }
+}
+
+- (IBAction)contactDate:(id)sender
+{
+    selectedCall = CONTACT_CALL;
+    
+    if(self.dateTextField.text.length > 0)
+    {
+        [self callForDate];
+    }
+    else
+    {
+        [self callForLimit];
+    }
+}
+
+- (IBAction)listDate:(id)sender
+{
+    selectedCall = LIST_CALL;
+    if(self.dateTextField.text.length > 0)
+    {
+        [self callForDate];
+    }
+    else
+    {
+        [self callForLimit];
+    }
+}
+
+- (IBAction)EmailCampaignDate:(id)sender
+{
+    selectedCall = EMAIL_CALL;
+    if(self.dateTextField.text.length > 0)
+    {
+        [self callForDate];
+    }
+    else
+    {
+        [self callForLimit];
+    }
+}
+
+- (IBAction)membershipDate:(id)sender
+{
+    selectedCall = MEMBERSHIP_CALL;
+    if(self.dateTextField.text.length > 0)
+    {
+        [self callForDate];
+    }
+    else
+    {
+        [self callForLimit];
+    }
+}
+
+#pragma mark - call for parameters
+- (void)callForDate
+{
+    [self.view endEditing:YES];
+    self.statusLable.text = status;
+    [loadingView showLoadingInView:self.view];
+    dispatch_queue_t callService = dispatch_queue_create("callService", nil);
+    dispatch_async(callService, ^{
+        
+        HttpResponse *response = nil;
+        
+        switch (selectedCall) {
+            case CONTACT_CALL: response = [ContactsCollection contactsWithAccessToken:[CTCTGlobal shared].token andModificationDate:[self.datePicker date]];
+                break;
+            case LIST_CALL: response = [ListsCollection listsWithAccessToken:[CTCTGlobal shared].token andModificationDate:[self.datePicker date]];
+                break;
+                
+            case EMAIL_CALL: response = [EmailCampaignService getCampaignsWithToken:[CTCTGlobal shared].token andModificationDate:[self.datePicker date]];
+                break;
+                
+            case MEMBERSHIP_CALL: response = [ListsCollection getContactListMembershipWithAccessToken:[CTCTGlobal shared].token fromList:@"1876010237" withModificationDate:[self.datePicker date] withAlimitOf:nil];
+                break;
+        
+            default: break;
+        }
+      
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            [loadingView hideLoading];
+            if(selectedCall != LIST_CALL)
+            {
+                ResultSet *set = response.data;
+                resultsArray = set.results;
+            }
+            else
+                resultsArray = response.data;
+            
+            if(response.errors.count > 0)
+            {
+                HttpError *err = [response.errors objectAtIndex:0];
+                [[[UIAlertView alloc] initWithTitle:@"Error" message:err.message delegate:self cancelButtonTitle:@"Ok" otherButtonTitles: nil] show];
+            }
+            else if(resultsArray.count == 0)
+                [[[UIAlertView alloc]initWithTitle:@"" message:@"No Results match this criteria" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil] show];
+            
+            [self.tableView reloadData];
+        });
+    });
+    dispatch_release(callService);
+}
+
+- (void)callForLimit
+{
+    [self.view endEditing:YES];
+    self.statusLable.text = status;
+    [loadingView showLoadingInView:self.view];
+    dispatch_queue_t callService = dispatch_queue_create("callService", nil);
+    dispatch_async(callService, ^{
+        
+        HttpResponse *response = nil;
+        
+        switch (selectedCall) {
+            case CONTACT_CALL: response = [ContactsCollection contactsWithAccessToken:[CTCTGlobal shared].token withLimitOf:self.textField.text];
+                break;
+                
+            case EMAIL_CALL: response = [EmailCampaignService getCampaignsWithToken:[CTCTGlobal shared].token withALimitOf:self.textField.text];
+                break;
+                
+            case MEMBERSHIP_CALL: response = [ListsCollection getContactListMembershipWithAccessToken:[CTCTGlobal shared].token fromList:@"1876010237" withModificationDate:nil withAlimitOf:self.textField.text];
+                break;
+                
+            default:break;
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            [loadingView hideLoading];
+            ResultSet *set = response.data;
+            resultsArray = set.results;
+            
+            if(response.errors.count > 0)
+            {
+                HttpError *err = [response.errors objectAtIndex:0];
+                [[[UIAlertView alloc] initWithTitle:@"Error" message:err.message delegate:self cancelButtonTitle:@"Ok" otherButtonTitles: nil] show];
+            }
+            else if(resultsArray.count == 0)
+                [[[UIAlertView alloc]initWithTitle:@"" message:@"No Results match this criteria" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil] show];
+            
+            [self.tableView reloadData];
+        });
+    });
+    dispatch_release(callService);
 }
 @end
